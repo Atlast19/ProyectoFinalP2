7ca7eb69d54da1784580f3b6a3c3b4fd76f4b938
Atlast19 <ahleandro18@gmail.com>
2025-08-11 09:52:36 -0400

    DTOs

---
+++ Aplicacion/DTOs/UsuarioDTO.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Aplicacion.DTOs
+{
+    public class UsuarioDTO
+    {
+        public string NameUser { get; set; }
+        public string PasswordUser { get; set; }
+        public string MatriculaUser { get; set; }
+        public string AgeUser { get; set; }
+        public string EmailUser { get; set; }
+    }
+}
